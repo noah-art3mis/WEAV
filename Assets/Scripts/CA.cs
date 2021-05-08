9ce900225ce7c09a8156e29d605912dcaadfcd23
noah-art3mis <costa.araujo.gustavo@gmail.com>
2021-05-08 12:21:10 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/CA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CA : MonoBehaviour
+{
+    Vector2 screenSize;
+    int[] cells;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
