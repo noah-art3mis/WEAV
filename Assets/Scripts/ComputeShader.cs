7c7dd1aed0941d4cefedb7be65f5219d9aac04c0
noah-art3mis <costa.araujo.gustavo@gmail.com>
2021-06-04 00:19:06 -0300

    :(

---
+++ Assets/Scripts/ComputeShader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ComputeShader : MonoBehaviour
+{
+    [SerializeField] private ComputeShader computeShader;
+    ComputeBuffer positionsBuffer;
+    private CA ca;
+
+    private void OnEnable()
+    {
+        ca = GetComponent<CA>();
+        positionsBuffer = new ComputeBuffer(ca.arraySize * ca.maxGenerations, 2 * 4);
+        computeShader.setBuffer();
+        computeShader.Dispatch();
+    }
+
+    private void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+}
